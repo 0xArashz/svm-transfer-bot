5a2c6503f6671b0a7123d789910ce8c3441daac4
Arash Pirhayati <ArashPirhayati1381@gmail.com>
2024-08-15 11:51:19 +0330

    Completed

--- README.md
+++ README.md
@@ -1,2 +1,56 @@
-# svm-transfer-bot
-SVM Transfer Bot can transfer random amount of gas token or native coin of SVM blockchains automatically from a wallet that you want to other wallets
+## SVM Transfer Bot
+
+SVM Transfer Bot can transfer random amount of gas token or native coin (between 0.00001 to 0.0001) of SVM blockchains (mainnet and testnet) automatically from a wallet that you want to other wallets (randomly interval between transfers 10s to 15s). I join many cryptocurrency airdrops and I usually use such bots that I code for projects that have testnet tasks (for example, the Sonic project recently).
+
+
+### Installation
+
+1. Clone the repository:
+
+   ```bash
+   git clone https://github.com/0xArashz/svm-transfer-bot.git
+   ```
+
+2. Navigate into the project directory:
+
+   ```bash
+   cd svm-transfer-bot
+   ```
+
+3. Install dependencies:
+
+   ```bash
+   npm install
+   ```
+
+4. Prepare input files:
+
+   - Create `.env` in the root and replace the `RPC_URL` and `PRIVATE_KEY` values:
+
+   ```bash
+   RPC_URL=SVM_BLOCKCHAIN_RPC_URL
+   PRIVATE_KEY=SENDER_WALLET_PRIVATE_KEY
+   ```
+
+   - Create `recipients.json` in the root with an array of recipients SVM addresses (as many as you want):
+
+   ```json
+   [
+     "RECIPENT_ADDRESS_01",
+     "RECIPENT_ADDRESS_02",
+     "RECIPENT_ADDRESS_03"
+   ]
+   ```
+
+
+### Usage
+
+Run bot:
+
+```bash
+npm start
+```
+
+<div>
+    <img src="https://github.com/user-attachments/assets/6e4f7a73-c7b9-40f6-b55b-cdcc23d11631" width="100%">
+</div>
---
+++ index.js
@@ -0,0 +1,64 @@
+const fs = require("fs");
+const bs58 = require("bs58");
+const web3 = require("@solana/web3.js");
+require("dotenv").config();
+
+
+const rpcURL = process.env.RPC_URL;
+const connection = new web3.Connection(rpcURL, "confirmed");
+const privateKey = process.env.PRIVATE_KEY;
+const recipients = JSON.parse(
+    fs.readFileSync("./recipients.json", "UTF-8")
+);
+const senderPublicKey = web3.Keypair.fromSecretKey(bs58.decode(privateKey));
+const recipientsPublicKey = recipients.map(
+    address => new web3.PublicKey(address)
+);
+
+const getRandomAmount = (min, max) => {
+    return (Math.random() * (max - min) + min).toFixed(8);
+};
+
+const getRandomDelay = () => {
+    return Math.floor(Math.random() * 6 + 10) * 1000;
+};
+
+let txCount = 0;
+const sendTransaction = async () => {
+    try {
+        for (let recipientPublicKey of recipientsPublicKey) {
+            const lamportsToSend = Math.floor(web3.LAMPORTS_PER_SOL * getRandomAmount(0.00001, 0.0001));
+            const txData = new web3.Transaction().add(
+                web3.SystemProgram.transfer({
+                    fromPubkey: senderPublicKey.publicKey,
+                    toPubkey: recipientPublicKey,
+                    lamports: lamportsToSend,
+                })
+            );
+            await web3.sendAndConfirmTransaction(connection, txData, [senderPublicKey]);
+            txCount++;
+            console.log(
+                "\x1b[37m%s\x1b[0m",
+                `--> #${txCount < 10 ? "0" + txCount : txCount} TRANSFER ${(lamportsToSend * Math.pow(10, -9)).toFixed(8)} COIN TO ${recipientPublicKey} DONE \u2705`
+            );
+            if (txCount !== recipientsPublicKey.length) {
+                const interval = getRandomDelay();
+                setTimeout(sendTransaction, interval);
+            } else {
+                console.log("\n");
+                process.exit();
+            }
+        }
+    } catch (error) {
+        console.error("\x1b[31m%s\x1b[0m", "SOMETHING WENT WRONG!");
+    }
+};
+
+process.stdout.write("\x1Bc");
+console.log("\x1b[37m%s\x1b[0m", "##################################################");
+console.log("\x1b[37m%s\x1b[0m", "#                                                #");
+console.log("\x1b[37m%s\x1b[0m", "#             DEVELOPER: 0xArashz                #");
+console.log("\x1b[37m%s\x1b[0m", "#     GITHUB: https://github.com/0xArashz        #");
+console.log("\x1b[37m%s\x1b[0m", "#                                                #");
+console.log("\x1b[37m%s\x1b[0m", "##################################################\n\n");
+sendTransaction();
